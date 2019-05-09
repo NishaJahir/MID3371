a63d119f83da76b3e9e9666e21aa1a9e84c01962
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-05-09 18:49:46 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -206,7 +206,7 @@ class PaymentService
 		
 	    $customerComments = $this->sessionStorage->getPlugin()->getValue('customerWish');
 	    $this->sessionStorage->getPlugin()->setValue('customerWish', null);
-            $transactionComments = $customerComments . PHP_EOL . $this->getTransactionComments($requestData);
+           // $transactionComments = $customerComments . PHP_EOL . $this->getTransactionComments($requestData);
 	    $this->getLogger(__METHOD__)->error('transactioncomments', $transactionComments);
             $this->paymentHelper->createPlentyPayment($requestData);
             $this->paymentHelper->updateOrderStatus((int)$requestData['order_no'], $requestData['order_status']);
