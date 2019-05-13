003722e353850b18bbb8c86ef02758a4780eda6b
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-05-13 18:23:41 +0530

    Update NovalnetOrderConfirmationDataProvider.php

--- src/Providers/NovalnetOrderConfirmationDataProvider.php
+++ src/Providers/NovalnetOrderConfirmationDataProvider.php
@@ -47,6 +47,19 @@ class NovalnetOrderConfirmationDataProvider
 		$barzhlentoken = '';
 		$barzahlenurl = '';
 		$payments = $paymentRepositoryContract->getPaymentsByOrderId($order['id']);
+		foreach ($paymentDetails as $paymentDetail)
+		{
+			$property = $paymentDetail->properties;
+			foreach($property as $proper)
+			{
+			  if ($proper->typeId == 22)
+				  {
+					$com = $proper->value;
+				  }	
+			}
+		}
+		
+		
 		$paymentHelper->logger('method', $payments);
 		if (!empty ($order['id'])) {
 			foreach($payments as $payment)
@@ -76,7 +89,7 @@ class NovalnetOrderConfirmationDataProvider
 						//$string_data = (string)$data->text;
 						//if(strpos($string_data, 'nn_check') == false){
 							$comment .= (string)$data->text;
-						$comment .= 'test';
+						$comment .= $com;
 						//$comment .= $com;
 							$comment .= PHP_EOL;
 						//}
