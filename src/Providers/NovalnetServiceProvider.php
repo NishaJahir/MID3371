39376f52b68a48fa1e0e7a9af33b709d370554c4
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-08-12 16:01:26 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -395,6 +395,7 @@ class NovalnetServiceProvider extends ServiceProvider
 		$order = $event->getOrder();
 		$document_type = $event->getDocType();
 		$payments = $paymentRepository->getPaymentsByOrderId( $order->id);
+		     $this->getLogger(__METHOD__)->error('pay', $payments);
 		$paymentKey = $paymentHelper->getPaymentKeyByMop($payments[0]->mopId);
 		  $this->getLogger(__METHOD__)->error('check1', $paymentKey);  
 		if (in_array ($paymentKey, ['NOVALNET_INVOICE', 'NOVALNET_PREPAYMENT']) ) {
